456a4abe38d031ce06fdc4a7a901548587b7fb03
duke9000 <duke.programmazione@gmail.com>
2025-10-16 23:50:08 +0200

    Unifica stile admin: usa pippo.css e percorsi relativi

--- public/admin.php
+++ public/admin.php
@@ -12,8 +12,8 @@ $user_name = $_SESSION['user_name'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin - Dashboard</title>
-    <link rel="stylesheet" href="/app/login/style/login.css"> <!-- opzionale -->
-    <link rel="icon" href="/app/login/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../style/admin.css"> <!-- usa pippo.css come richiesto -->
+    <link rel="icon" href="../img/favicon.ico" type="image/x-icon">
 </head>
 <body>
     <h1>📦 Benvenuto, <?php echo htmlspecialchars($user_name); ?>!</h1>
@@ -22,10 +22,10 @@ $user_name = $_SESSION['user_name'];
     <a href="magazzino.php">Magazzino</a>
     <a href="cucina.php" style="margin-right:12px;">Cucina</a>
     <a href="ufficio.php" style="margin-right:12px;">Ufficio</a>
-    <a href="dati_inseriti.php" style="margin-right:12px;">Dati inseriti</a>
+    <a href="admin_viste.php" style="margin-right:12px;">Dati inseriti</a>
     </nav>
 
     <p>Questa è la tua pagina dedicata al <strong>tutto</strong>.</p>
-    <a href="/app/login/logout.php">Logout</a>
+    <a href="../logout.php">Logout</a>
 </body>
 </html>
